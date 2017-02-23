f16ed34d667ff04ea940262dc6d28865f40e0e4b
Jeremy Kun <jeremy@21.co>
2017-02-22 16:21:30 -0800

    write the plugin

--- index.js
+++ index.js
@@ -6,46 +6,46 @@ module.exports = function (options) {
   var t = options.types;
 
   const updateConfigReferenceVisitor = {
-    visitor: {
-      MemberExpression: function MemberExpression(path, state) {
-        // If Config.VALUE is on the LHS of an assignment statement, ignore.
-        // However, note that doing such could result in differing behavior
-        // between Transpiled and compiled use of react-native-config, because
-        // "assigned" Config values would be ignored as all references would be
-        // transpiled to raw values.
-        if(t.isAssignmentExpression(path.parent) && path.parent.left == path.node) return;
+    MemberExpression(path, state) {
+      // If Config.VALUE is on the LHS of an assignment statement, ignore.
+      // However, note that doing such could result in differing behavior
+      // between Transpiled and compiled use of react-native-config, because
+      // "assigned" Config values would be ignored as all references would be
+      // transpiled to raw values.
+      if(t.isAssignmentExpression(path.parent) && path.parent.left == path.node) {
+        return;
+      }
 
-        if (path.get("object").matchesPattern(this.import_name)) {
-          if (!dotenv) {
-            dotenv = require('dotenv').config(state.opts);
-          }
-          var key = path.toComputedKey();
-          if (t.isStringLiteral(key)) {
-            var name = key.value;
-            var value = (state.opts.env && name in state.opts.env) ? state.opts.env[name] : process.env[name];
-            path.replaceWith(t.valueToNode(value));
-          }
+      var object = path.node.object;
+      var property = path.node.property;
+      if (object.name == this.import_name) {
+        if (!dotenv) {
+          dotenv = require('dotenv').config(state.opts);
         }
+
+        var name = property.name;
+        var overrideEnv = state.opts && state.opts.env && name in state.opts.env;
+        var replaceValue = overrideEnv ? state.opts.env[name] : process.env[name];
+        path.replaceWith(t.valueToNode(replaceValue));
       }
     }
   };
 
   return {
     visitor: {
-      File: function File(path) {
+      Program: function Program(path) {
         var import_name;
 
-        if (path.node.program && path.node.program.body) {
-          path.node.program.body.foreach() {
-            if (this.type == "ImportDeclaration" && this.source.value == "react-native-config" && 
-                this.specifiers && this.specifiers.length > 0) {
-              var specifier = this.specifiers[0];
+        if (path.node.body) {
+          path.node.body.forEach(function(childNode) {
+            if (childNode.type == "ImportDeclaration" && childNode.source.value == "react-native-config" && 
+                childNode.specifiers && childNode.specifiers.length > 0) {
+              var specifier = childNode.specifiers[0];
               if (specifier && specifier.type == "ImportDefaultSpecifier") {
                 import_name = specifier.local.name;
-                this.remove();
               }
             }
-          }
+          });
         }
 
         path.traverse(updateConfigReferenceVisitor, { import_name });
