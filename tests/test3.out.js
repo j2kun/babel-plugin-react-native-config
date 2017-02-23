3ce774d6e003e7e00c42f3d321e6ffc45be83041
Jeremy Kun <jeremy@21.co>
2017-02-22 17:22:33 -0800

    update tests

--- tests/test3.out.js
+++ tests/test3.out.js
@@ -2,7 +2,6 @@ import Config from 'react-native-config';
 
 var API_URL = 'foo' || 'api.my-default-url.com';
 var WWW_URL = 'bar' || 'my-default-web-url.biz';
-var FONT = 'baz' || 'Helvetica';
 var defaultFun = true;
-var ENABLE_FUN = undefined.charAt(0) in ['1', 't', 'T'] ? true : defaultFun;
+var ENABLE_FUN = undefined.charAt(0) in ['1', 't', 'T'] ? true : defaultFun; // watch out for undefined
 var NAVBAR_HEIGHT = parseInt('65') || 64;
